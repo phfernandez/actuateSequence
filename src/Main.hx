20b8571d244c3c0a85f1aedf6251810f4c9f4505
phfernandez <46404993+phfernandez@users.noreply.github.com>
2019-01-05 20:10:14 +0100

    Add files via upload

---
+++ src/Main.hx
@@ -0,0 +1,149 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.display.Shape;
+import openfl.events.Event;
+import openfl.events.MouseEvent;
+import openfl.Lib;
+import openfl.text.TextField;
+import openfl.text.TextFieldAutoSize;
+import openfl.text.TextFormat;
+
+import motion.Actuate;
+import motion.easing.*;
+
+import tweening.ActuateSequence;
+
+/**
+ * ...
+ * @author Pedro Fernandez
+ */
+class Main extends Sprite 
+{
+	
+	private var seq:ActuateSequence;
+	
+	public function new() 
+	{
+		super();
+		
+		var squareSize:Int;
+		
+		squareSize = 100;
+		var square1 = new Shape ();
+		square1.graphics.beginFill (0xFF0000, 0.5);
+		square1.graphics.drawRect (0, 0, squareSize, squareSize);
+		square1.graphics.endFill ();
+		var sprite1:Sprite = new Sprite();
+		square1.x = -square1.width / 2;
+		square1.y = -square1.height / 2;
+		sprite1.addChild (square1);
+		sprite1.x = 150;
+		sprite1.y = 150;
+		addChild(sprite1);
+		sprite1.name = "sprite1";
+		
+		squareSize = 200;
+		var square2 = new Shape ();
+		square2.graphics.beginFill (0x00FF00, 0.5);
+		square2.graphics.drawRect (0, 0, squareSize, squareSize);
+		square2.graphics.endFill ();
+		var sprite2:Sprite = new Sprite();
+		square2.x = -square2.width / 2;
+		square2.y = -square2.height / 2;
+		sprite2.addChild (square2);
+		sprite2.x = 400;
+		sprite2.y = 200;
+		addChild(sprite2);
+		sprite2.name = "sprite2";
+		
+		squareSize = 75;
+		var square3 = new Shape ();
+		square3.graphics.beginFill (0x0000FF, 0.5);
+		square3.graphics.drawRect (0, 0, squareSize, squareSize);
+		square3.graphics.endFill ();
+		var sprite3:Sprite = new Sprite();
+		square3.x = -square3.width / 2;
+		square3.y = -square3.height / 2;
+		sprite3.addChild (square3);
+		sprite3.x = 700;
+		sprite3.y = 200;
+		addChild(sprite3);
+		sprite3.name = "sprite3";
+		
+		
+		var textFormat:TextFormat = new TextFormat("Arial", 24, 0x000000);
+		
+		var playText = new TextField ();
+		playText.defaultTextFormat = textFormat;
+		playText.autoSize = TextFieldAutoSize.LEFT;
+		playText.text = "Play";
+		playText.x = 20;
+		playText.y = stage.stageHeight - 40;
+		addChild (playText);
+		playText.addEventListener(MouseEvent.CLICK, onClickPlayButton);
+		
+		var pauseText = new TextField ();
+		pauseText.defaultTextFormat = textFormat;
+		pauseText.autoSize = TextFieldAutoSize.LEFT;
+		pauseText.text = "Pause";
+		pauseText.x = playText.x + playText.width + 30;
+		pauseText.y = stage.stageHeight - 40;
+		addChild (pauseText);
+		pauseText.addEventListener(MouseEvent.CLICK, onClickPauseButton);
+		
+		var stopText = new TextField ();
+		stopText.defaultTextFormat = textFormat;
+		stopText.autoSize = TextFieldAutoSize.LEFT;
+		stopText.text = "Stop";
+		stopText.x = pauseText.x + pauseText.width + 30;
+		stopText.y = stage.stageHeight - 40;
+		addChild (stopText);
+		stopText.addEventListener(MouseEvent.CLICK, onClickStopButton);
+		
+		
+		
+		// ActuateSequence definition in Array
+		var aSequence = new Array<Dynamic>();
+		aSequence = [
+			//{ target:getAsset("titulo"), duration:0.5, from: { alpha:0.0, x:"-=50" }, to: { alpha:1.0, x:"original", delay:0.25 }  },
+			
+			{ target:sprite1, duration:0.5, from: { alpha:0.0, x:"-=50", delay:0.25 } },
+			{ target:sprite2, duration:0.5, from: { alpha:0.0, scaleX:2.0, scaleY:2.0 }, to: { alpha:1.0, scaleX:1.0, scaleY:1.0, delay: -0.25 } },
+			{ target:sprite3, duration:0.75, from: { y:"+=150", rotation:60, delay: -0.25, ease:Elastic.easeOut } },
+			
+			{ target:sprite1, duration:0.5, to: { y:"+=400", delay:0.0, ease:Quad.easeIn } },
+			{ target:sprite3, duration:0.5, to: { alpha:0.0, scaleX:3.0, scaleY:3.0, delay: -0.25, ease:Expo.easeIn } },
+			{ target:sprite2, duration:1.0, to: { alpha:0.0, scaleX:2.0, scaleY:2.0, rotation:"+=200", delay: -0.25, ease:Expo.easeIn } }
+		];
+		
+		seq = new ActuateSequence();
+		seq.addEventListener( ActuateSequence.ON_COMPLETE_SEQUENCE, endSequence);
+		seq.init(aSequence); // Init sequence first time
+		
+		seq.start(); // Start from the beginning
+		
+	}
+	
+	private function endSequence(e:Event = null):Void
+	{
+		//trace("endSequence");
+	}
+	
+	
+	function onClickPlayButton(e:MouseEvent):Void 
+	{
+		if (seq != null) seq.play();
+	}
+	
+	function onClickStopButton(e:MouseEvent):Void 
+	{
+		if (seq != null) seq.stop();
+	}
+	
+	function onClickPauseButton(e:MouseEvent):Void 
+	{
+		if (seq != null) seq.pause();
+	}
+
+}
---
+++ src/tweening/ActuateSequence.hx
@@ -0,0 +1,450 @@
+package tweening;
+
+import openfl.display.DisplayObject;
+import openfl.display.MovieClip;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.events.EventDispatcher;
+
+import motion.Actuate;
+import motion.easing.*;
+
+/**
+ * ...
+ * @author Pedro Fernandez
+ */
+class ActuateSequence extends EventDispatcher
+{
+	private var aUsedTargets:Map<String,Dynamic>;
+	private var aOriginalTargets:Map<String,Dynamic>;
+	private var aSequence:Array<Dynamic>;
+	private var actuateFromProps:Dynamic;
+	private var actuateToProps:Dynamic;
+	public var playing:Bool = false;
+	public var paused:Bool = false;
+	
+	public inline static var ON_COMPLETE_SEQUENCE:String = "ON_COMPLETE_SEQUENCE";
+	
+	
+	
+	public function new() 
+	{
+		super();
+		aOriginalTargets = new Map<String,Dynamic>();
+	}
+	
+	
+	
+	// ---------------------------------------------------------------------------------------------------------------------------
+	// Function init: Pass aSequence Array first time; Call without arguments to initialize/replay sequence
+	// ---------------------------------------------------------------------------------------------------------------------------
+	public function init(aSequence:Array<Dynamic> = null):Void
+	{
+		if (aSequence != null) 
+		{
+			aOriginalTargets = new Map<String,Dynamic>();
+			this.aSequence = aSequence;
+		}
+		
+		var tl_line:Dynamic;
+		var tl_global_delay:Float = 0.0;
+		var onCompleteFunction:Dynamic;
+		
+		aUsedTargets = new Map<String,Dynamic>(); // Init used targets each time the timeline initializes
+		
+		for (i in 0...this.aSequence.length)
+		{
+			
+			tl_line = this.aSequence[i]; // Get current line of timeline with properties: target, duration, from, to
+			
+			// Default tween values
+			var duration = Reflect.field(tl_line, "duration");
+			var delay = 0.0;
+			var ease = Sine.easeOut;
+			
+			var val:Dynamic = 0.0;
+			var rel_val:Float = 0.0;
+			
+			var target:DisplayObject = cast( Reflect.field(tl_line, "target"), DisplayObject );
+			var targetUsed:Bool = false;
+			var originalTargetData:Dynamic;
+			
+			var from_data:Dynamic = Reflect.field(tl_line, "from");
+			var to_data:Dynamic = Reflect.field(tl_line, "to");
+			var fields_from:Array<String> = Reflect.fields ( from_data );
+			var fields_to:Array<String> = Reflect.fields ( to_data );
+			var from_alpha:Dynamic = 0.0;
+			var to_alpha:Dynamic = 1.0;
+			
+			var has_from_data:Bool = false;
+			
+			// Init target with original values and save in Map table 
+			if ( !aOriginalTargets.exists (target.name) )
+			{
+				var targetFromData:Dynamic = { };
+				Reflect.setField (targetFromData, "target", target);
+				Reflect.setField (targetFromData, "orig_x", target.x);
+				Reflect.setField (targetFromData, "orig_y", target.y);
+				Reflect.setField (targetFromData, "orig_scaleX", target.scaleX);
+				Reflect.setField (targetFromData, "orig_scaleY", target.scaleY);
+				Reflect.setField (targetFromData, "orig_rotation", target.rotation);
+				Reflect.setField (targetFromData, "orig_alpha", target.alpha);
+				
+				if (from_data != null)
+				{
+					has_from_data = true;
+					
+					for (field in Reflect.fields (from_data)) {
+						
+						if ( field != "delay" && field != "ease" )
+						{
+							val = Reflect.field(from_data, field);
+							if ( Std.is(val, String) )
+							{
+								if (val.indexOf("-=") != -1) 
+								{
+									val = Reflect.getProperty (target, field) - Std.parseFloat ( (val.split("-=")[1]) );
+								}
+								else if (val.indexOf("+=") != -1) 
+								{
+									val = Reflect.getProperty (target, field) + Std.parseFloat ( (val.split("+=")[1]) );
+								}
+							}
+							
+							Reflect.setField (targetFromData, "from_" + field, val);
+						}
+						
+					}
+					
+				}
+				
+				Reflect.setField (targetFromData, "has_from_data", has_from_data);
+				
+				// Save in Map table
+				aOriginalTargets.set ( target.name, targetFromData );
+				
+			}
+			
+			// Save target if previosly used in timeline
+			if ( !aUsedTargets.exists (target.name) ) aUsedTargets.set (target.name, true); else targetUsed = true; // Mark target as used
+			
+			originalTargetData = aOriginalTargets.get(target.name);
+			
+			if (!targetUsed)
+			{
+				// Set target to initial positions
+				for (field in Reflect.fields (originalTargetData)) 
+				{
+					if ( field.indexOf("orig_") != -1 )
+					{
+						val = Reflect.field(originalTargetData, field);
+						Reflect.setProperty(target, field.split("orig_")[1], val);
+					}
+					
+				}
+				
+			}
+			
+			
+			
+			if (from_data != null)
+			{
+				
+				actuateFromProps = {};
+				for (propertyFromName in fields_from) 
+				{
+					if ( propertyFromName == "delay" )
+					{
+						delay = Reflect.field(from_data, propertyFromName);
+					}
+					else if ( propertyFromName == "ease" )
+					{
+						ease = Reflect.field(from_data, propertyFromName);
+					}
+					else
+					{
+						val = Reflect.field(from_data, propertyFromName);
+						if ( Std.is(val, String) )
+						{
+							if (val.indexOf("-=") != -1) 
+							{
+								val = Reflect.getProperty (target, propertyFromName) - Std.parseFloat ( (val.split("-=")[1]) );
+							}
+							else if (val.indexOf("+=") != -1) 
+							{
+								val = Reflect.getProperty (target, propertyFromName) + Std.parseFloat ( (val.split("+=")[1]) );
+							}
+						}
+						
+						Reflect.setField (actuateFromProps, propertyFromName, val);
+					}
+					
+				}
+				
+				Actuate.apply ( target, actuateFromProps );
+				
+			}
+			
+			
+			val = 0.0;
+			actuateToProps = { };
+			
+			if (to_data != null)
+			{
+
+				for (propertyToName in fields_to) 
+				{
+					
+					if ( propertyToName == "delay" )
+					{
+						delay = Reflect.field(to_data, propertyToName);
+					}
+					else if ( propertyToName == "ease" )
+					{
+						ease = Reflect.field(to_data, propertyToName);
+					}
+					else
+					{
+						val = Reflect.field(to_data, propertyToName);
+						if ( Std.is(val, String) )
+						{
+							if (val.indexOf("-=") != -1) 
+							{
+								val = Reflect.getProperty (target, propertyToName) - Std.parseFloat ( (val.split("-=")[1]) );
+							}
+							else if (val.indexOf("+=") != -1) 
+							{
+								val = Reflect.getProperty (target, propertyToName) + Std.parseFloat ( (val.split("+=")[1]) );
+							}
+						}
+						
+						Reflect.setField (actuateToProps, propertyToName, val);
+					}
+					
+				}
+				
+			}
+			else
+			{
+				
+				for (field in Reflect.fields (originalTargetData)) 
+				{
+					if ( field.indexOf("orig_") != -1 )
+					{
+						val = Reflect.field(originalTargetData, field);
+						Reflect.setField(actuateToProps, field.split("orig_")[1], val);
+					}
+					
+				}
+				
+				if (from_data != null)
+				{
+					from_alpha = Reflect.field(from_data, "alpha");
+					if ( Std.is(from_alpha, Float) )
+					{
+						if ( from_alpha < 1.0 ) to_alpha = 1.0;
+					}
+				}
+				else
+				{
+					to_alpha = originalTargetData.orig_alpha;
+				}
+				
+				Reflect.setField(actuateToProps, "alpha", to_alpha);
+				
+			}
+			
+			if ( i == 0) tl_global_delay += delay; else tl_global_delay += delay + duration;
+			if ( i == this.aSequence.length - 1 ) onCompleteFunction = endSequence; else onCompleteFunction = null;
+			
+			Actuate.tween ( target, duration, actuateToProps, false ).delay(tl_global_delay).ease(ease).onComplete(onCompleteFunction);
+			
+		}
+		
+		// Stop all this sequence targets after timeline creation
+		pauseSequenceTargets();
+		//Actuate.pauseAll();
+		playing = false;
+		paused = false;
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------------------------
+	// Function reset: Resets all objects from this sequence animation to the starting positions
+	// ---------------------------------------------------------------------------------------------------------------------------
+	public function reset():Void
+	{
+		playing = false;
+		paused = false;
+		//Actuate.reset();
+		
+		var target:DisplayObject;
+		var originalTargetData:Dynamic;
+		var val:Dynamic = 0.0;
+		
+		if (aSequence != null) 
+		{
+			//init();
+			
+			
+			
+			for ( target_name in aOriginalTargets.keys() ) 
+			{
+				
+				originalTargetData = aOriginalTargets.get(target_name);
+				target = originalTargetData.target;
+				
+				
+				// Reset original displayObject properties
+				for (field in Reflect.fields (originalTargetData)) 
+				{
+					if ( field.indexOf("orig_") != -1 )
+					{
+						val = Reflect.field(originalTargetData, field);
+						if (field == "orig_alpha") 
+						{
+							target.visible = true;
+						}
+						Reflect.setProperty(target, field.split("orig_")[1], val);
+					}
+					
+				}
+				
+				if (originalTargetData.has_from_data)
+				{
+					
+					// Reset "from" displayObject properties
+					for (field in Reflect.fields (originalTargetData)) 
+					{
+						
+						if ( field.indexOf("from_") != -1 )
+						{
+							if ( field != "has_from_data" )
+							{
+								val = Reflect.field(originalTargetData, field);
+								if (field == "from_alpha") 
+								{
+									target.visible = true;
+								}
+								Reflect.setProperty(target, field.split("from_")[1], val);
+							}
+							
+						}
+						
+					}
+					
+				}
+				
+				
+			}
+			
+			
+			
+		}
+	}
+	
+	private function pauseSequenceTargets():Void
+	{
+		var originalTargetData:Dynamic;
+		var target:DisplayObject;
+		for ( target_name in aOriginalTargets.keys() ) 
+		{
+			originalTargetData = aOriginalTargets.get(target_name);
+			target = originalTargetData.target;
+			Actuate.pause(target);
+		}
+	}
+	
+	private function stopSequenceTargets():Void
+	{
+		var originalTargetData:Dynamic;
+		var target:DisplayObject;
+		for ( target_name in aOriginalTargets.keys() ) 
+		{
+			originalTargetData = aOriginalTargets.get(target_name);
+			target = originalTargetData.target;
+			Actuate.stop(target);
+		}
+	}
+	
+	private function resumeSequenceTargets():Void
+	{
+		var originalTargetData:Dynamic;
+		var target:DisplayObject;
+		for ( target_name in aOriginalTargets.keys() ) 
+		{
+			originalTargetData = aOriginalTargets.get(target_name);
+			target = originalTargetData.target;
+			Actuate.resume(target);
+		}
+	}
+	
+	
+	// ---------------------------------------------------------------------------------------------------------------------------
+	// Function start: Play sequence animation always from the beginning
+	// ---------------------------------------------------------------------------------------------------------------------------
+	public function start():Void
+	{
+		init();
+		
+		playing = true;
+		paused = false;
+		
+		resumeSequenceTargets();
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------------------------
+	// Function play: Play sequence animation from the beginning or resumes playback from current position (if paused)
+	// ---------------------------------------------------------------------------------------------------------------------------
+	public function play():Void
+	{
+		if (!playing && !paused)
+		{
+			init();
+		}
+		playing = true;
+		paused = false;
+		
+		resumeSequenceTargets();
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------------------------
+	// Function pause: Pauses sequence animation
+	// ---------------------------------------------------------------------------------------------------------------------------
+	public function pause():Void
+	{
+		if (playing)
+		{
+			playing = false;
+			paused = true;
+			
+			pauseSequenceTargets();
+		}
+		
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------------------------
+	// Function stop: Stop sequence animation resetting values to first frame (similar to gotoAndPlay(0) in gsap timeline)
+	// ---------------------------------------------------------------------------------------------------------------------------
+	public function stop():Void
+	{
+		playing = false;
+		paused = false;
+		
+		stopSequenceTargets();
+		reset();
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------------------------
+	// Function endSequence: Event called onComplete; 
+	// Dispatches event ON_COMPLETE_SEQUENCE to control end of timeline from outside
+	// ---------------------------------------------------------------------------------------------------------------------------
+	private function endSequence():Void
+	{
+		playing = false;
+		paused = false;
+		
+		dispatchEvent( new Event(ActuateSequence.ON_COMPLETE_SEQUENCE) );
+	}
+	
+	
+}
